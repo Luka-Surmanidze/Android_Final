1238fd997f74c3f5814593b7a457d287a831574c
Givi_Modebadze <gmode22@freeuni.edu.ge>
2025-08-02 13:04:47 +0400

    feature: user goes to homepage directly if they were logged in previously.

--- app/src/main/java/ge/gmodebadze/android_final/MainActivity.kt
+++ app/src/main/java/ge/gmodebadze/android_final/MainActivity.kt
@@ -1,15 +1,31 @@
 package ge.gmodebadze.android_final
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import ge.gmodebadze.android_final.databinding.FragmentRegisterBinding
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.NavController
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        if (currentUser != null && navController.currentDestination?.id == R.id.loginFragment) {
+            navController.navigate(R.id.action_loginFragment_to_homeFragment)
+        }
     }
 }
